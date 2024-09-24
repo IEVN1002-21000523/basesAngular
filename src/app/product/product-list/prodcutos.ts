38007e7685436f91c2b3d9b836a2e955cee49dd6
Galilea Ferrer porras <sakuramamilla@gmail.com>
2024-09-24 12:56:45 -0600

    sabado 21

---
+++ src/app/product/product-list/prodcutos.ts
@@ -0,0 +1,12 @@
+export interface IProductos
+{
+  "productoID":3,
+  "Modelo":"Rio",
+  "Descripcion":"4 puertas",
+  "Precio":150000,
+  "Year":2020,
+  "Marca":"KIA",
+  "Color":"Azul",
+  "ImagenUrl":""
+
+}
